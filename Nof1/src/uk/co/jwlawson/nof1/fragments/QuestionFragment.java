8d5d94c375fb39a58092d8744005eb9136dde16b
John Lawson <john@jwlawson.co.uk>
2012-07-03 17:30:35 +0100

    Setting up recalling data from saved states (bundles) so the user can rotate the view.
    Apparently it is frowned upon to use anything other than a default
    constructor for Fragments. oops.
    
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Nof1/src/uk/co/jwlawson/nof1/fragments/QuestionFragment.java
+++ Nof1/src/uk/co/jwlawson/nof1/fragments/QuestionFragment.java
@@ -1,37 +1,77 @@
 package uk.co.jwlawson.nof1.fragments;
 
+import uk.co.jwlawson.nof1.BuildConfig;
+import uk.co.jwlawson.nof1.R;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
-import uk.co.jwlawson.nof1.R;
 
 public class QuestionFragment extends SherlockFragment {
-	
+
+	public static int COUNT = 0;
+
 	private String mQuestion;
 	private String mMin;
 	private String mMax;
-	
+	private int mId;
+
 	public QuestionFragment(String question, String min, String max) {
 		mQuestion = question;
 		mMin = min;
 		mMax = max;
+		mId = COUNT;
+		COUNT++;
 	}
-	
+
+	public QuestionFragment() {
+		if (BuildConfig.DEBUG) Log.d(getClass().getName(), "New QuestionFragment" + mQuestion);
+		mId = COUNT;
+		COUNT++;
+	}
+
+	@Override
+	public void onCreate(Bundle savedState) {
+		if (savedState.containsKey("QuesFrag" + mId + "Question"))
+			mQuestion = savedState.getString("QuesFrag" + mId + "Question");
+		if (savedState.containsKey("QuesFrag" + mId + "Min"))
+			mMin = savedState.getString("QuesFrag" + mId + "Min");
+		if (savedState.containsKey("QuesFrag" + mId + "Max"))
+			mMax = savedState.getString("QuesFrag" + mId + "Max");
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		
+
 		View view = inflater.inflate(R.layout.mock_row_layout_data_input, container, false);
-		
+
 		((TextView) view.findViewById(R.id.textView1)).setText(mQuestion);
 		((TextView) view.findViewById(R.id.textView2)).setText(mMin);
 		((TextView) view.findViewById(R.id.textView3)).setText(mMax);
-		
+
 		return view;
-		
+
 	}
-	
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		// For each questionFragment, add its strings to the bundle
+		// Use Id to differentiate.
+		outState.putString("QuesFrag" + mId + "Question", mQuestion);
+		outState.putString("QuesFrag" + mId + "Min", mMin);
+		outState.putString("QuesFrag" + mId + "Max", mMax);
+	}
+
+	@Override
+	public void setArguments(Bundle savedState) {
+		mQuestion = savedState.getString("QuesFrag" + mId + "Question");
+		mMin = savedState.getString("QuesFrag" + mId + "Min");
+		mMax = savedState.getString("QuesFrag" + mId + "Max");
+	}
+
 }
