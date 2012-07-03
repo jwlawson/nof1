c40e158c8dd535d2e792b5d835a7f666e939079f
John Lawson <john@jwlawson.co.uk>
2012-07-03 11:25:59 +0100

    Adding a second column for data input on large screen devices.
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Nof1/src/uk/co/jwlawson/nof1/activities/FragActivity.java
+++ Nof1/src/uk/co/jwlawson/nof1/activities/FragActivity.java
@@ -19,14 +19,40 @@ public class FragActivity extends SherlockFragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mock_layout_data_input);
 		
 		Fragment frag1 = new QuestionFragment("On average, in comparison to your usual episodes, how long were the attacks?", "Very long", "Very short");
-		Fragment frag2 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks", "Very bad", "Very mild");
+		Fragment frag2 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
+		Fragment frag3 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
+		Fragment frag4 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
+		Fragment frag5 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
 		
 		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 		ft.add(R.id.data_input_fragment_layout, frag1, "fragment1");
 		ft.add(R.id.data_input_fragment_layout, frag2, "fragment2");
+		ft.add(R.id.data_input_fragment_layout, frag3, "fragment3");
+		ft.add(R.id.data_input_fragment_layout, frag4, "fragment4");
+		ft.add(R.id.data_input_fragment_layout, frag5, "fragment5");
 		ft.commit();
+		
+		if (findViewById(R.id.data_input_fragment_layout2) != null) {
+			
+			if (BuildConfig.DEBUG) Log.d(this.getClass().getName(), "Dual column view found");
+			
+			Fragment frag6 = new QuestionFragment("On average, in comparison to your usual episodes, how long were the attacks?", "Very long", "Very short");
+			Fragment frag7 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
+			Fragment frag8 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
+			Fragment frag9 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
+			Fragment frag0 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
+			
+			FragmentTransaction ft2 = getSupportFragmentManager().beginTransaction();
+			ft2.add(R.id.data_input_fragment_layout2, frag6, "fragment6");
+			ft2.add(R.id.data_input_fragment_layout2, frag7, "fragment7");
+			ft2.add(R.id.data_input_fragment_layout2, frag8, "fragment8");
+			ft2.add(R.id.data_input_fragment_layout2, frag9, "fragment9");
+			ft2.add(R.id.data_input_fragment_layout2, frag0, "fragment0");
+			ft2.commit();
+		}
 		
 		if (BuildConfig.DEBUG) Log.d(this.getClass().getName(), "Fragments created and added to view");
 	}
