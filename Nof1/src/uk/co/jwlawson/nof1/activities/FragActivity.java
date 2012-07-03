160ca98b1cf11f931f646437b931be082683207c
John Lawson <john@jwlawson.co.uk>
2012-07-03 16:15:05 +0100

    Added AsyncTask to load fragments into UI.
    Now has a set of 4 different questions which are loaded to fragments on
    create, each with accompanying min and max descriptions. The fragments
    are created and added to LinearLayouts in the AsyncTask.
    
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Nof1/src/uk/co/jwlawson/nof1/activities/FragActivity.java
+++ Nof1/src/uk/co/jwlawson/nof1/activities/FragActivity.java
@@ -3,58 +3,78 @@ package uk.co.jwlawson.nof1.activities;
 import uk.co.jwlawson.nof1.BuildConfig;
 import uk.co.jwlawson.nof1.R;
 import uk.co.jwlawson.nof1.fragments.QuestionFragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
 public class FragActivity extends SherlockFragmentActivity {
-	
+
 	public FragActivity() {
-		
+
 	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mock_layout_data_input);
-		
-		Fragment frag1 = new QuestionFragment("On average, in comparison to your usual episodes, how long were the attacks?", "Very long", "Very short");
-		Fragment frag2 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
-		Fragment frag3 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
-		Fragment frag4 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
-		Fragment frag5 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
-		
-		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-		ft.add(R.id.data_input_fragment_layout, frag1, "fragment1");
-		ft.add(R.id.data_input_fragment_layout, frag2, "fragment2");
-		ft.add(R.id.data_input_fragment_layout, frag3, "fragment3");
-		ft.add(R.id.data_input_fragment_layout, frag4, "fragment4");
-		ft.add(R.id.data_input_fragment_layout, frag5, "fragment5");
-		ft.commit();
-		
-		if (findViewById(R.id.data_input_fragment_layout2) != null) {
-			
-			if (BuildConfig.DEBUG) Log.d(this.getClass().getName(), "Dual column view found");
-			
-			Fragment frag6 = new QuestionFragment("On average, in comparison to your usual episodes, how long were the attacks?", "Very long", "Very short");
-			Fragment frag7 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
-			Fragment frag8 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
-			Fragment frag9 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
-			Fragment frag0 = new QuestionFragment("On average, in comparison with your usual episodes, how severe were the attacks?", "Very bad", "Very mild");
-			
-			FragmentTransaction ft2 = getSupportFragmentManager().beginTransaction();
-			ft2.add(R.id.data_input_fragment_layout2, frag6, "fragment6");
-			ft2.add(R.id.data_input_fragment_layout2, frag7, "fragment7");
-			ft2.add(R.id.data_input_fragment_layout2, frag8, "fragment8");
-			ft2.add(R.id.data_input_fragment_layout2, frag9, "fragment9");
-			ft2.add(R.id.data_input_fragment_layout2, frag0, "fragment0");
-			ft2.commit();
+
+		// I think that if the state is saved, fragments are kept
+		// Need to try this out
+		if (savedInstanceState == null) {
+			// Load fragments in data input view in background
+			new FragLoader().execute();
 		}
-		
-		if (BuildConfig.DEBUG) Log.d(this.getClass().getName(), "Fragments created and added to view");
+
+		if (BuildConfig.DEBUG)
+			Log.d(this.getClass().getName(), "Fragments created and added to view");
 	}
-	
+
+	private class FragLoader extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+
+			// Will read these from the config file
+			String[] qus = new String[] {
+					"On average, in comparison to your usual episodes, how long were the attacks?",
+					"On average, in comparison with your usual episodes, how severe were the attacks?",
+					"On average how difficult do you find it to climb the stairs?",
+					"How breathless are you after walking continuously for 5 minutes?" };
+			String[] min = new String[] { "Very long", "Very bad", "Very hard", "Very breathless" };
+			String[] max = new String[] { "Very short", "Very mild", "Very easy", "Not at all" };
+
+			// Check whether the device is large enough for 2 columns
+			boolean dualCol = (findViewById(R.id.data_input_fragment_layout2) != null);
+			if (dualCol) Log.d("FragAct", "Dual columns found");
+
+			for (int i = 0; i < qus.length; i++) {
+
+				FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+				ft.add(R.id.data_input_fragment_layout,
+						new QuestionFragment(qus[i], min[i], max[i]), "fragment" + i);
+				if (dualCol)
+					ft.add(R.id.data_input_fragment_layout2, new QuestionFragment(qus[qus.length
+							- 1 - i], min[qus.length - 1 - i], max[qus.length - 1 - i]),
+							"fragment2" + i);
+				ft.commit();
+			}
+
+			for (int i = 0; i < qus.length; i++) {
+
+				FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+				ft.add(R.id.data_input_fragment_layout,
+						new QuestionFragment(qus[i], min[i], max[i]), "fragment" + i);
+				if (dualCol)
+					ft.add(R.id.data_input_fragment_layout2, new QuestionFragment(qus[qus.length
+							- 1 - i], min[qus.length - 1 - i], max[qus.length - 1 - i]),
+							"fragment2" + i);
+				ft.commit();
+			}
+			return null;
+		}
+
+	}
 }
