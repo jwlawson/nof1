3aecf96fdb165b5cd36d6a75a7c1fc236fffb06f
John Lawson <john@jwlawson.co.uk>
2012-07-04 15:40:42 +0100

    Fixed setting arguments for retaining fragments.
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Nof1/src/uk/co/jwlawson/nof1/fragments/NumberFragment.java
+++ Nof1/src/uk/co/jwlawson/nof1/fragments/NumberFragment.java
@@ -1,7 +1,9 @@
 package uk.co.jwlawson.nof1.fragments;
 
+import uk.co.jwlawson.nof1.BuildConfig;
 import uk.co.jwlawson.nof1.R;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +13,13 @@ import android.widget.TextView;
 import com.actionbarsherlock.app.SherlockFragment;
 
 public class NumberFragment extends SherlockFragment {
+	private static final String TAG = "NumberFragment";
 
 	private static int COUNT = 0;
 	private String mQuestion;
 	private EditText mText;
 	private String mNumber;
+	private boolean init = false;
 	private int mId;
 
 	public NumberFragment() {
@@ -32,18 +36,23 @@ public class NumberFragment extends SherlockFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if (!init) mySetArguments(savedInstanceState);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-		View view = inflater.inflate(R.layout.row_layout_data_number, container);
+		if (!init) mySetArguments(savedInstanceState);
+
+		View view = inflater.inflate(R.layout.row_layout_data_number, container, false);
 
 		TextView text = (TextView) view.findViewById(R.id.data_input_number_text);
 		text.setText(mQuestion);
 
 		mText = (EditText) view.findViewById(R.id.data_input_number_edittext);
 		if (mNumber != null) mText.setText(mNumber);
+
+		if (BuildConfig.DEBUG) Log.d(TAG, mId + " view created");
 
 		return view;
 	}
@@ -58,9 +67,14 @@ public class NumberFragment extends SherlockFragment {
 	@Override
 	public void setArguments(Bundle args) {
 		super.setArguments(args);
+		mySetArguments(args);
+	}
+
+	private void mySetArguments(Bundle args) {
 		if (args != null) {
 			mQuestion = args.getString("NumFrag" + mId + "Question");
 			mNumber = args.getString("NumFrag" + mId + "Number");
+			init = true;
 		}
 	}
 }
