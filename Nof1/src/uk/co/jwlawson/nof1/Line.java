e75d205173bd635e3899cdfeb8374f32ac7ff609
John Lawson <john@jwlawson.co.uk>
2012-07-06 15:27:42 +0100

    Added debug logging.
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Nof1/src/uk/co/jwlawson/nof1/Line.java
+++ Nof1/src/uk/co/jwlawson/nof1/Line.java
@@ -22,6 +22,7 @@ package uk.co.jwlawson.nof1;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.util.Log;
 
 /**
  * Simple class for string and drawing a line
@@ -30,20 +31,30 @@ import android.graphics.Paint;
  * 
  */
 public class Line {
-	
+
+	private static final boolean DEBUG = false;
+
 	private Vec2 start;
 	private Vec2 end;
-	
+
 	public Line(Vec2 start, Vec2 end) {
 		this.start = start;
 		this.end = end;
 	}
-	
+
 	public void draw(Canvas c, Paint paint) {
+		if (DEBUG)
+			Log.d("Line",
+					"drawing line " + start.getX() + " , " + start.getY() + " , " + end.getX()
+							+ " , " + end.getY());
 		c.drawLine(start.getX(), start.getY(), end.getX(), end.getY(), paint);
 	}
-	
+
 	public void draw(Canvas c, float scaleX, float scaleY, Paint paint) {
-		c.drawLine(start.getX() * scaleX, start.getY() * scaleY, end.getX() * scaleX, end.getY() * scaleY, paint);
+		if (DEBUG)
+			Log.d("Line", "drawing line " + start.getX() * scaleX + " , " + start.getY() * scaleY
+					+ " , " + end.getX() * scaleX + " , " + end.getY() * scaleY);
+		c.drawLine(start.getX() * scaleX, start.getY() * scaleY, end.getX() * scaleX, end.getY()
+				* scaleY, paint);
 	}
 }
