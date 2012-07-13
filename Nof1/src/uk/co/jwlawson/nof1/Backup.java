f229de1d2ebce3f4f450fc16cd4ca1155042185f
John Lawson <john@jwlawson.co.uk>
2012-07-13 11:40:43 +0100

    Basic backup agent implemented. Needs extending to backup database.
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

---
+++ Nof1/src/uk/co/jwlawson/nof1/Backup.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Nof1 Trials helper, making life easier for clinicians and patients in N of 1 trials.
+ * Copyright (C) 2012  WMG, University of Warwick
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You may obtain a copy of the GNU General Public License at 
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     John Lawson - initial API and implementation
+ ******************************************************************************/
+package uk.co.jwlawson.nof1;
+
+import java.io.IOException;
+
+import android.annotation.TargetApi;
+import android.app.backup.BackupAgentHelper;
+import android.app.backup.BackupDataInput;
+import android.app.backup.BackupDataOutput;
+import android.app.backup.SharedPreferencesBackupHelper;
+import android.os.ParcelFileDescriptor;
+
+/**
+ * @author John Lawson
+ * 
+ */
+@TargetApi(8)
+public class Backup extends BackupAgentHelper {
+
+	private static final String BACKUP = "prefs";
+
+	public Backup() {
+	}
+
+	// Allocate a helper and add it to the backup agent
+	@Override
+	public void onCreate() {
+		SharedPreferencesBackupHelper helper = new SharedPreferencesBackupHelper(this, Keys.CONFIG_NAME, Keys.QUES_NAME, Keys.DEFAULT_PREFS);
+		addHelper(BACKUP, helper);
+
+	}
+
+	@Override
+	public void onBackup(ParcelFileDescriptor oldState, BackupDataOutput data, ParcelFileDescriptor newState) throws IOException {
+		super.onBackup(oldState, data, newState);
+
+		// TODO backup database
+	}
+
+	@Override
+	public void onRestore(BackupDataInput data, int appVersionCode, ParcelFileDescriptor newState) throws IOException {
+		super.onRestore(data, appVersionCode, newState);
+
+		// TODO rebuild database
+	}
+
+}
