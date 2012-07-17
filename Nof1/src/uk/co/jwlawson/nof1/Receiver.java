8f1760a93caf8375e06581cddaf7525584527615
John Lawson <john@jwlawson.co.uk>
2012-07-17 11:07:58 +0100

    Cleaning up notification code
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Nof1/src/uk/co/jwlawson/nof1/Receiver.java
+++ Nof1/src/uk/co/jwlawson/nof1/Receiver.java
@@ -28,6 +28,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -39,132 +40,129 @@ import android.util.Log;
  * 
  */
 public class Receiver extends BroadcastReceiver {
-
+	
 	private static final String TAG = "Receiver";
 	private static final boolean DEBUG = true && BuildConfig.DEBUG;
-
+	
 	/**
 	 * 
 	 */
 	public Receiver() {
 	}
-
+	
 	@Override
 	public void onReceive(Context context, Intent intent) {
-
+		
 		// intent.getAction() could be null
 		if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
 			if (DEBUG) Log.d(TAG, "Boot_complete broadcast caught");
-
+			
 			Intent i = new Intent(context, Scheduler.class);
 			i.putExtra(Keys.INTENT_BOOT, true);
 			context.startService(i);
-
+			
 		} else if (intent.getBooleanExtra(Keys.INTENT_ALARM, false)) {
 			if (DEBUG) Log.d(TAG, "AlarmManager alarm caught");
-
+			
 			// Show notification
-			setNotification(context);
-
+			setRepeatNotification(context);
+			
 			// Pass to scheduler
 			Intent i = new Intent(context, Scheduler.class);
 			i.putExtra(Keys.INTENT_BOOT, false);
 			i.putExtra(Keys.INTENT_ALARM, true);
 			context.startService(i);
-
+			
 		} else if (intent.getBooleanExtra(Keys.INTENT_FIRST, false)) {
 			if (DEBUG) Log.d(TAG, "First time run alarm caught");
-
+			
 			// Show first run notification
 			setFirstNotification(context);
-
+			
 			// Pass to scheduler
 			Intent i = new Intent(context, Scheduler.class);
 			i.putExtra(Keys.INTENT_BOOT, false);
-			i.putExtra(Keys.INTENT_ALARM, true); // Really want to use INTENT_ALARM, not INTENT_FIRST
+			i.putExtra(Keys.INTENT_ALARM, true); // Really do want to use INTENT_ALARM, not INTENT_FIRST
 			context.startService(i);
 		}
 	}
-
-	private void setNotification(Context context) {
-
-		SharedPreferences sp = context.getSharedPreferences(Keys.DEFAULT_PREFS, Context.MODE_PRIVATE);
-
+	
+	/** Set notification reminding patient to input data */
+	private void setRepeatNotification(Context context) {
 		Intent intent = new Intent(context, Questionnaire.class);
 		intent.putExtra(Keys.INTENT_PREVIEW, false);
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-		PendingIntent pi = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-
-		NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-		builder.setContentIntent(pi).setContentTitle("N-of-1 Trials").setContentText("Please fill in the questionnnaire").setAutoCancel(true)
-				.setSmallIcon(R.drawable.ic_noti).setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_noti));
-
-		if (sp.getBoolean(Keys.DEFAULT_LOUD, false)) {
-			// Notification makes noise
-			builder.setDefaults(Notification.DEFAULT_SOUND);
-			if (DEBUG) Log.d(TAG, "Loud Notification");
-		}
-
-		if (sp.getBoolean(Keys.DEFAULT_FLASH, false)) {
-			// Notification to flash
-			builder.setDefaults(Notification.DEFAULT_LIGHTS);
-			if (DEBUG) Log.d(TAG, "Flashy Notification");
-		}
-
-		if (sp.getBoolean(Keys.DEFAULT_VIBE, false)) {
-			// Notification to vibrate
-			builder.setDefaults(Notification.DEFAULT_VIBRATE);
-			if (DEBUG) Log.d(TAG, "Vibrating Notification");
-		}
-
-		Notification noti = builder.getNotification();
-
-		((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE)).notify(0x100, noti);
-
-		if (DEBUG) Log.d(TAG, "Notification posted");
-
+		
+		setNotification(context, intent, R.string.noti_repeat_title, R.string.noti_repeat_text);
 	}
-
+	
+	/** Set the first notification at start of trial */
 	private void setFirstNotification(Context context) {
-
-		SharedPreferences sp = context.getSharedPreferences(Keys.DEFAULT_PREFS, Context.MODE_PRIVATE);
-
-		// TODO Load home screen
+		
 		Intent intent = new Intent(context, Questionnaire.class);
 		intent.putExtra(Keys.INTENT_PREVIEW, false);
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
+		
+		setNotification(context, intent, R.string.noti_first_title, R.string.noti_repeat_text);
+		
+	}
+	
+	/**
+	 * Make and post a notification with specified params
+	 * 
+	 * @param context
+	 * @param intent Intent to fire on notification click
+	 * @param title Title of notification
+	 * @param text Text in notification
+	 */
+	private void setNotification(Context context, Intent intent, String title, String text) {
+		
+		SharedPreferences sp = context.getSharedPreferences(Keys.DEFAULT_PREFS, Context.MODE_PRIVATE);
+		
 		PendingIntent pi = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-
+		
 		NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-		builder.setContentIntent(pi).setContentTitle("N-of-1 Trials").setContentText("Your trial is due to start today").setAutoCancel(true)
-				.setSmallIcon(R.drawable.ic_noti);
-
+		builder.setContentIntent(pi).setContentTitle(title).setContentText(text).setAutoCancel(true).setSmallIcon(R.drawable.ic_noti)
+				.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_noti));
+		
 		if (sp.getBoolean(Keys.DEFAULT_LOUD, false)) {
 			// Notification makes noise
 			builder.setDefaults(Notification.DEFAULT_SOUND);
 			if (DEBUG) Log.d(TAG, "Loud Notification");
 		}
-
+		
 		if (sp.getBoolean(Keys.DEFAULT_FLASH, false)) {
 			// Notification to flash
 			builder.setDefaults(Notification.DEFAULT_LIGHTS);
 			if (DEBUG) Log.d(TAG, "Flashy Notification");
 		}
-
+		
 		if (sp.getBoolean(Keys.DEFAULT_VIBE, false)) {
 			// Notification to vibrate
 			builder.setDefaults(Notification.DEFAULT_VIBRATE);
 			if (DEBUG) Log.d(TAG, "Vibrating Notification");
 		}
-
+		
 		Notification noti = builder.getNotification();
-
+		
 		((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE)).notify(0x100, noti);
-
+		
 		if (DEBUG) Log.d(TAG, "Notification posted");
-
+		
 	}
-
+	
+	/**
+	 * Make and post a notification with specified params
+	 * 
+	 * @param context
+	 * @param intent Intent to fire on notification click
+	 * @param title Resource with title of notification
+	 * @param text Resource with text in notification
+	 */
+	private void setNotification(Context context, Intent intent, int title, int text) {
+		Resources res = context.getResources();
+		String titleStr = res.getString(title);
+		String textStr = res.getString(text);
+		setNotification(context, intent, titleStr, textStr);
+	}
 }
