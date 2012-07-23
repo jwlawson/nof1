83a0cce6286dba32060bff4fa25d9e3f881e7df4
John Lawson <john@jwlawson.co.uk>
2012-07-23 18:17:14 +0100

    Fixed comment loading, so it actually loads comments
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Nof1/src/uk/co/jwlawson/nof1/activities/CommentList.java
+++ Nof1/src/uk/co/jwlawson/nof1/activities/CommentList.java
@@ -2,12 +2,18 @@ package uk.co.jwlawson.nof1.activities;
 
 import java.util.ArrayList;
 
+import uk.co.jwlawson.nof1.BuildConfig;
+import uk.co.jwlawson.nof1.DataSource;
 import uk.co.jwlawson.nof1.R;
+import uk.co.jwlawson.nof1.SQLite;
 import uk.co.jwlawson.nof1.fragments.CommentDetailFragment;
 import uk.co.jwlawson.nof1.fragments.CommentListFragment;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
@@ -15,12 +21,19 @@ import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.Window;
 
-public class CommentList extends SherlockFragmentActivity implements CommentListFragment.OnListItemSelectedListener,
-		CommentListFragment.OnListItemAddedListener {
+public class CommentList extends SherlockFragmentActivity implements CommentListFragment.OnListItemSelectedListener, CommentListFragment.OnListItemAddedListener {
 	
+	private static final String TAG = "CommentList";
+	private static final boolean DEBUG = true && BuildConfig.DEBUG;
+	
+	/** True if on device wide enough for two panels */
 	private boolean mTwoPane;
 	
+	/** List of all comments as strings */
 	private ArrayList<String> mList;
+	
+	/** Database handler */
+	private DataSource mData;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +48,8 @@ public class CommentList extends SherlockFragmentActivity implements CommentList
 		if (findViewById(R.id.comment_detail_container) != null) {
 			mTwoPane = true;
 		}
+		
+		new Loader().execute();
 	}
 	
 	@Override
@@ -49,12 +64,19 @@ public class CommentList extends SherlockFragmentActivity implements CommentList
 	
 	@Override
 	public void onListItemSelected(ListView l, View v, int position, long id) {
+		if (DEBUG) Log.d(TAG, "List item selected: " + position);
 		
 		if (mTwoPane) {
+			// Replace comment details fragment with new one
+			// TODO Check whether we need to replace it
 			CommentDetailFragment frag = CommentDetailFragment.newInstance(mList.get(position));
 			getSupportFragmentManager().beginTransaction().replace(R.id.comment_detail_container, frag).commit();
 			
 		} else {
+			// Clear selection from carrying on when return from new actiity
+			((CommentListFragment) getSupportFragmentManager().findFragmentById(R.id.comment_list)).clearSelected();
+			
+			// Load new activity
 			Intent detailIntent = new Intent(this, CommentDetail.class);
 			detailIntent.putExtra(CommentDetailFragment.ARG_COMMENT, mList.get(position));
 			startActivity(detailIntent);
@@ -64,5 +86,43 @@ public class CommentList extends SherlockFragmentActivity implements CommentList
 	@Override
 	public void onListItemAdded(String comment, String date) {
 		mList.add(comment);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// Make sure database is closed if open
+		if (mData != null) mData.close();
+	}
+	
+	private class Loader extends AsyncTask<Void, Void, Cursor> {
+		
+		@Override
+		protected void onPreExecute() {
+			setSupportProgressBarIndeterminateVisibility(true);
+			super.onPreExecute();
+		}
+		
+		@Override
+		protected Cursor doInBackground(Void... params) {
+			if (DEBUG) Log.d(TAG, "Loading cursor");
+			
+			// Open database
+			mData = new DataSource(CommentList.this);
+			mData.open();
+			
+			// Quesry database
+			Cursor cursor = mData.getColumn(SQLite.COLUMN_COMMENT);
+			
+			return cursor;
+		}
+		
+		@Override
+		protected void onPostExecute(Cursor result) {
+			// Set list cursor
+			((CommentListFragment) getSupportFragmentManager().findFragmentById(R.id.comment_list)).setCursor(result);
+			
+			super.onPostExecute(result);
+		}
 	}
 }
--- Nof1/src/uk/co/jwlawson/nof1/fragments/CommentListFragment.java
+++ Nof1/src/uk/co/jwlawson/nof1/fragments/CommentListFragment.java
@@ -3,6 +3,7 @@ package uk.co.jwlawson.nof1.fragments;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import uk.co.jwlawson.nof1.BuildConfig;
 import uk.co.jwlawson.nof1.Keys;
 import uk.co.jwlawson.nof1.SQLite;
 import android.app.Activity;
@@ -11,10 +12,14 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class CommentListFragment extends FragList {
+	
+	private static final String TAG = "CommentListFragment";
+	private static final boolean DEBUG = true && BuildConfig.DEBUG;
 	
 	private Cursor mCursor;
 	
@@ -64,6 +69,12 @@ public class CommentListFragment extends FragList {
 		new Loader().execute();
 	}
 	
+	@Override
+	public void onDestroy() {
+		if (mCursor != null) mCursor.close();
+		super.onDestroy();
+	}
+	
 	private class Loader extends AsyncTask<Void, String, Void> {
 		
 		@Override
@@ -74,6 +85,7 @@ public class CommentListFragment extends FragList {
 		
 		@Override
 		protected Void doInBackground(Void... params) {
+			if (DEBUG) Log.d(TAG, "Loading values from cursor");
 			
 			mCursor.moveToFirst();
 			
@@ -102,6 +114,9 @@ public class CommentListFragment extends FragList {
 				String date = cal.get(Calendar.DAY_OF_MONTH) + "/" + (cal.get(Calendar.MONTH) + 1) + "/" + cal.get(Calendar.YEAR);
 				mListener.onListItemAdded(comment, date);
 				publishProgress(date);
+				
+				calDay = day;
+				mCursor.moveToNext();
 			}
 			return null;
 		}
@@ -121,6 +136,7 @@ public class CommentListFragment extends FragList {
 		protected void onPostExecute(Void result) {
 			super.onPostExecute(result);
 			getSherlockActivity().setSupportProgressBarIndeterminateVisibility(false);
+			if (DEBUG) Log.d(TAG, "Data loaded");
 		}
 		
 	}
