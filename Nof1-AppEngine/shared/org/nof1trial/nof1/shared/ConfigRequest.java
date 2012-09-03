fe9b9aea93c13a8d9682496fd8ec220773056323
John Lawson <john@jwlawson.co.uk>
2012-09-03 16:52:28 +0100

    Adds new empty files

---
+++ Nof1-AppEngine/shared/org/nof1trial/nof1/shared/ConfigRequest.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Nof1 Trials helper, making life easier for clinicians and patients in N of 1 trials.
+ * Copyright (C) 2012  John Lawson
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You may obtain a copy of the GNU General Public License at 
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     John Lawson - initial API and implementation
+ ******************************************************************************/
+package org.nof1trial.nof1.shared;
+
+import com.google.web.bindery.requestfactory.shared.RequestContext;
+
+/**
+ * @author John Lawson
+ * 
+ */
+public interface ConfigRequest extends RequestContext {
+
+}
