64cc78576d73a5f4f4afe311230fc07b2fb17789
John Lawson <john@jwlawson.co.uk>
2012-09-05 14:59:10 +0100

    Fixing git and eclipse links
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

---
+++ Nof1/shared/org/nof1trial/nof1/shared/ConfigProxy.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * Nof1 Trials helper, making life easier for clinicians and patients in N of 1 trials.
+ * Copyright (C) 2012  John Lawson
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You may obtain a copy of the GNU General Public License at 
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     John Lawson - initial API and implementation
+ ******************************************************************************/
+package org.nof1trial.nof1.shared;
+
+import com.google.web.bindery.requestfactory.shared.EntityProxy;
+import com.google.web.bindery.requestfactory.shared.ProxyForName;
+
+/**
+ * @author John Lawson
+ * 
+ */
+@ProxyForName(value = "org.nof1trial.nof1.server.Config")
+public interface ConfigProxy extends EntityProxy {
+
+	public String getPatientEmail();
+
+	public void setPatientEmail(String patientEmail);
+
+	public String getPatientName();
+
+	public void setPatientName(String name);
+
+	public String getDocEmail();
+
+	public void setDocEmail(String docEmail);
+
+	public String getDoctorName();
+
+	public void setDoctorName(String doctorName);
+
+	public String getPharmEmail();
+
+	public void setPharmEmail(String pharmEmail);
+
+	public String getTreatmentA();
+
+	public void setTreatmentA(String treatmentA);
+
+	public String getTreatmentB();
+
+	public void setTreatmentB(String treatmentB);
+
+	public String getTreatmentNotes();
+
+	public void setTreatmentNotes(String treatmentNotes);
+
+	public String getStartDate();
+
+	public void setStartDate(String startDate);
+
+	public Long getNumberPeriods();
+
+	public void setNumberPeriods(Long numberPeriods);
+
+	public Long getLengthPeriods();
+
+	public void setLengthPeriods(Long lengthPeriods);
+
+	public Long getId();
+
+	public Integer getVersion();
+
+}
---
+++ Nof1/shared/org/nof1trial/nof1/shared/ConfigRequest.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Nof1 Trials helper, making life easier for clinicians and patients in N of 1 trials.
+ * Copyright (C) 2012  John Lawson
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You may obtain a copy of the GNU General Public License at 
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     John Lawson - initial API and implementation
+ ******************************************************************************/
+package org.nof1trial.nof1.shared;
+
+import java.util.List;
+
+import com.google.web.bindery.requestfactory.shared.Request;
+import com.google.web.bindery.requestfactory.shared.RequestContext;
+import com.google.web.bindery.requestfactory.shared.ServiceName;
+
+/**
+ * @author John Lawson
+ * 
+ */
+@ServiceName("org.nof1trial.nof1.server.Config")
+public interface ConfigRequest extends RequestContext {
+
+	public Request<List<ConfigProxy>> findAllConfigs();
+
+	public Request<ConfigProxy> findConfig(Long id);
+
+	public Request<List<ConfigProxy>> findConfigByDoctor(String doctorEmail, int maxResults);
+
+	public Request<List<ConfigProxy>> findConfigByPatient(String patientEmail, int maxResults);
+
+	public Request<ConfigProxy> update(ConfigProxy config);
+
+	public Request<Void> delete(ConfigProxy config);
+
+}
