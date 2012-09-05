84ff1b1fc73c4c63573c61f85f58c4fec921a80c
John Lawson <john@jwlawson.co.uk>
2012-09-05 10:58:33 +0100

    Removing unwanted stuff from project generation
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

---
+++ Nof1-AppEngine/src/org/nof1trial/nof1/client/RequestEntryPoint.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Nof1 Trials helper, making life easier for clinicians and patients in N of 1 trials.
+ * Copyright (C) 2012  John Lawson
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You may obtain a copy of the GNU General Public License at 
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     John Lawson - initial API and implementation
+ ******************************************************************************/
+package org.nof1trial.nof1.client;
+
+import org.nof1trial.nof1.shared.MyRequestFactory;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.SimpleEventBus;
+
+/**
+ * @author John Lawson
+ * 
+ */
+public class RequestEntryPoint implements EntryPoint {
+
+	public RequestEntryPoint() {
+	}
+
+	@Override
+	public void onModuleLoad() {
+		final EventBus eventBus = new SimpleEventBus();
+
+		MyRequestFactory requestFactory = GWT.create(MyRequestFactory.class);
+
+		requestFactory.initialize(eventBus);
+
+	}
+
+}
