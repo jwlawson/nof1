db3e206179184f52c4f5d769c34fbc48e99dd386
John Lawson <john@jwlawson.co.uk>
2012-09-06 17:36:23 +0100

    Listens for change in connectivity.
    If change means device connected to internet then Saver it woken up to
    upload any outstanding data. The receiver is only enabled when Saver is
    waiting to send data.
    
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

---
+++ Nof1/src/org/nof1trial/nof1/NetworkChangeReceiver.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Nof1 Trials helper, making life easier for clinicians and patients in N of 1 trials.
+ * Copyright (C) 2012  John Lawson
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You may obtain a copy of the GNU General Public License at 
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     John Lawson - initial API and implementation
+ ******************************************************************************/
+package org.nof1trial.nof1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+/**
+ * Broadcast receiver that is disabled by default. Will be enabled if there is data to upload, so the app can wait until
+ * there is an internet connection, then upload data.
+ * 
+ * @author John Lawson
+ * 
+ */
+public class NetworkChangeReceiver extends BroadcastReceiver {
+
+	private static final String TAG = "NetworkChangeReceiver";
+	private static final boolean DEBUG = BuildConfig.DEBUG;
+
+	public NetworkChangeReceiver() {
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+			if (DEBUG) Log.d(TAG, "Network connection changed");
+
+			ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+			NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+			boolean isConnected = (activeNetwork == null ? false : activeNetwork.isConnectedOrConnecting());
+			if (DEBUG) Log.d(TAG, "Internet connected: " + isConnected);
+
+			if (isConnected) {
+				// Pass connectivity change to Saver
+				Intent saver = new Intent(context, Saver.class);
+				saver.setAction(ConnectivityManager.CONNECTIVITY_ACTION);
+				context.startService(saver);
+			}
+		}
+	}
+
+}
