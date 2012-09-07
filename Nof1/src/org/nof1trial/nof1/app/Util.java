79a6193f171a5a1865f2f3f1812c17f35eb99867
John Lawson <john@jwlawson.co.uk>
2012-09-07 11:35:01 +0100

    Adds refresh auth cookie method
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Nof1/src/org/nof1trial/nof1/app/Util.java
+++ Nof1/src/org/nof1trial/nof1/app/Util.java
@@ -22,12 +22,31 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.params.HttpClientParams;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.nof1trial.nof1.Keys;
+import org.nof1trial.nof1.activities.AccountsActivity;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.AssetManager;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.google.web.bindery.event.shared.SimpleEventBus;
@@ -59,11 +78,22 @@ public class Util {
 	public static final String AUTH_COOKIE = "authCookie";
 
 	/**
-	 * Key for device registration id in shared preferences.
+	 * Cookie name for authorization.
 	 */
-	public static final String DEVICE_REGISTRATION_ID = "deviceRegistrationID";
+	private static final String AUTH_COOKIE_NAME = "SACSID";
 
-	/*
+	/**
+	 * The AppEngine app name, used to construct the production service URL
+	 * below.
+	 */
+	private static final String APP_NAME = "nof1trial";
+
+	/**
+	 * The URL of the production service.
+	 */
+	public static final String PROD_URL = "https://" + APP_NAME + ".appspot.com";
+
+	/**
 	 * URL suffix for the RequestFactory servlet.
 	 */
 	public static final String RF_METHOD = "/gwtRequest";
@@ -78,7 +108,7 @@ public class Util {
 	/**
 	 * Key for shared preferences.
 	 */
-	private static final String SHARED_PREFS = "ACCOUNT_PREFS";
+	private static final String SHARED_PREFS = Keys.CONFIG_NAME;
 
 	/**
 	 * Cache containing the base URL for a given context.
@@ -98,7 +128,7 @@ public class Util {
 			// otherwise, use the production url
 			if (url == null) {
 				if (DEBUG) Log.d(TAG, "Using production url");
-				url = Setup.PROD_URL;
+				url = Util.PROD_URL;
 			}
 			URL_MAP.put(context, url);
 		}
@@ -134,7 +164,98 @@ public class Util {
 	 * Helper method to get a SharedPreferences instance.
 	 */
 	public static SharedPreferences getSharedPreferences(Context context) {
-		return context.getSharedPreferences(SHARED_PREFS, 0);
+		return context.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+	}
+
+	/**
+	 * Refresh the auth cookie stored in shared prefs.Will then call invlidateAuthToken in account manager, so
+	 * 
+	 * @param context
+	 */
+	public static void refreshAuthCookie(Context context) {
+		// Get account name from prefs
+		final SharedPreferences prefs = Util.getSharedPreferences(context);
+		String accountName = prefs.getString(Util.ACCOUNT_NAME, "error");
+
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putString(Util.AUTH_COOKIE, null);
+		editor.commit();
+
+		if (Util.isDebug(context)) {
+			// Use a fake cookie for the dev mode app engine server
+			// The cookie has the form email:isAdmin:userId
+			// We set the userId to be the same as the account name
+			String authCookie = "dev_appserver_login=" + accountName + ":false:" + accountName;
+			prefs.edit().putString(Util.AUTH_COOKIE, authCookie).commit();
+		}
+
+		// Obtain an auth token and register
+		final AccountManager mgr = AccountManager.get(context);
+		Account[] accts = mgr.getAccountsByType("com.google");
+		for (final Account acct : accts) {
+			if (acct.name.equals(accountName)) {
+				if (Util.isDebug(context)) {
+					// Use a fake cookie for the dev mode app engine server
+					// The cookie has the form email:isAdmin:userId
+					// We set the userId to be the same as the account name
+					String authCookie = "dev_appserver_login=" + accountName + ":false:" + accountName;
+					prefs.edit().putString(Util.AUTH_COOKIE, authCookie).commit();
+				} else {
+					// Get the auth token from the AccountManager and convert
+					// it into a cookie for the appengine server
+
+					// This bit is a bit of a hack, as need to ask AccountManager for the old authToken, then invalidate
+					// it before requesting a new authToken.
+					// Unfortunately this leads to nested callbacks.
+					final AccountsActivity act = new AccountsActivity();
+					// TODO Check that 'ah' is the correct auth type, might be 'android'
+					mgr.getAuthToken(acct, "ah", null, act, new AccountManagerCallback<Bundle>() {
+						public void run(AccountManagerFuture<Bundle> future) {
+							try {
+								Bundle authTokenBundle = future.getResult();
+								String authToken = authTokenBundle.get(AccountManager.KEY_AUTHTOKEN).toString();
+
+								// Invalidate auth token, so next time Account Manager requests a new one
+								mgr.invalidateAuthToken("com.google", authToken);
+
+								// Request new one
+								mgr.getAuthToken(acct, "ah", null, act, new AccountManagerCallback<Bundle>() {
+									public void run(AccountManagerFuture<Bundle> future) {
+										try {
+											Bundle authTokenBundle = future.getResult();
+											String authToken = authTokenBundle.get(AccountManager.KEY_AUTHTOKEN).toString();
+											String authCookie = getAuthCookie(authToken);
+											prefs.edit().putString(Util.AUTH_COOKIE, authCookie).commit();
+
+										} catch (AuthenticatorException e) {
+											Log.w(TAG, "Got AuthenticatorException " + e);
+											Log.w(TAG, Log.getStackTraceString(e));
+										} catch (IOException e) {
+											Log.w(TAG, "Got IOException " + Log.getStackTraceString(e));
+											Log.w(TAG, Log.getStackTraceString(e));
+										} catch (OperationCanceledException e) {
+											Log.w(TAG, "Got OperationCanceledException " + e);
+											Log.w(TAG, Log.getStackTraceString(e));
+										}
+									}
+								}, null);
+
+							} catch (AuthenticatorException e) {
+								Log.w(TAG, "Got AuthenticatorException " + e);
+								Log.w(TAG, Log.getStackTraceString(e));
+							} catch (IOException e) {
+								Log.w(TAG, "Got IOException " + Log.getStackTraceString(e));
+								Log.w(TAG, Log.getStackTraceString(e));
+							} catch (OperationCanceledException e) {
+								Log.w(TAG, "Got OperationCanceledException " + e);
+								Log.w(TAG, Log.getStackTraceString(e));
+							}
+						}
+					}, null);
+				}
+				break;
+			}
+		}
 	}
 
 	/**
@@ -143,7 +264,7 @@ public class Util {
 	public static boolean isDebug(Context context) {
 		// Although this is a bit roundabout, it has the nice side effect
 		// of caching the result.
-		return !Setup.PROD_URL.equals(getBaseUrl(context));
+		return !Util.PROD_URL.equals(getBaseUrl(context));
 	}
 
 	/**
@@ -189,6 +310,44 @@ public class Util {
 		}
 
 		return url;
+	}
+
+	/**
+	 * Retrieves the authorization cookie associated with the given token. This
+	 * method should only be used when running against a production appengine
+	 * backend (as opposed to a dev mode server).
+	 */
+	private static String getAuthCookie(String authToken) {
+		try {
+			// Get SACSID cookie
+			DefaultHttpClient client = new DefaultHttpClient();
+			String continueURL = Util.PROD_URL;
+			URI uri = new URI(Util.PROD_URL + "/_ah/login?continue=" + URLEncoder.encode(continueURL, "UTF-8") + "&auth=" + authToken);
+			HttpGet method = new HttpGet(uri);
+			final HttpParams getParams = new BasicHttpParams();
+			HttpClientParams.setRedirecting(getParams, false);
+			method.setParams(getParams);
+
+			HttpResponse res = client.execute(method);
+			Header[] headers = res.getHeaders("Set-Cookie");
+			if (res.getStatusLine().getStatusCode() != 302 || headers.length == 0) {
+				return null;
+			}
+
+			for (Cookie cookie : client.getCookieStore().getCookies()) {
+				if (AUTH_COOKIE_NAME.equals(cookie.getName())) {
+					return AUTH_COOKIE_NAME + "=" + cookie.getValue();
+				}
+			}
+		} catch (IOException e) {
+			Log.w(TAG, "Got IOException " + e);
+			Log.w(TAG, Log.getStackTraceString(e));
+		} catch (URISyntaxException e) {
+			Log.w(TAG, "Got URISyntaxException " + e);
+			Log.w(TAG, Log.getStackTraceString(e));
+		}
+
+		return null;
 	}
 
 	/**
