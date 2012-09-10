9f0631de74d866e1e03d26cb743aebbc12c1052a
John Lawson <john@jwlawson.co.uk>
2012-09-10 12:04:12 +0100

    Adds header and some styling
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Nof1-AppEngine/src/org/nof1trial/nof1/client/RequestEntryPoint.java
+++ Nof1-AppEngine/src/org/nof1trial/nof1/client/RequestEntryPoint.java
@@ -28,10 +28,18 @@ import org.nof1trial.nof1.shared.ConfigProxy;
 import org.nof1trial.nof1.shared.DataProxy;
 import org.nof1trial.nof1.shared.MyRequestFactory;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.DecoratorPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.web.bindery.event.shared.EventBus;
 import com.google.web.bindery.event.shared.SimpleEventBus;
 import com.google.web.bindery.requestfactory.shared.Receiver;
@@ -44,7 +52,9 @@ public class RequestEntryPoint implements EntryPoint {
 
 	private MyRequestFactory mRequestFactory;
 
-	private VerticalPanel vPanel;
+	private FlowPanel vPanel;
+
+	private Label lblLoggedIn;
 
 	/** Comparator that sorts data by the time it is collected */
 	private static final Comparator<DataProxy> DATAPROXY_COMPARATOR = new Comparator<DataProxy>() {
@@ -83,10 +93,41 @@ public class RequestEntryPoint implements EntryPoint {
 			}
 		});
 
-		vPanel = new VerticalPanel();
-		vPanel.setWidth("100%");
+		vPanel = new FlowPanel();
+		vPanel.setStyleName("vPanel");
 
-		RootPanel.get().add(vPanel);
+		RootPanel rootPanel = RootPanel.get();
+		rootPanel.setSize("100%", "100%");
+		rootPanel.add(vPanel);
+
+		FlowPanel headerPanel = new FlowPanel();
+		headerPanel.setStyleName("headerPanel");
+		vPanel.add(headerPanel);
+
+		// Nof1 logo header
+		Image imgHeader = new Image("images/Nof1-Icon.png");
+		imgHeader.setStyleName("gwt-Image-header");
+		headerPanel.add(imgHeader);
+		imgHeader.setSize("128px", "128px");
+
+		// Header text
+		Label lblHeader = new Label("User data");
+		lblHeader.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
+		lblHeader.setWordWrap(false);
+		lblHeader.setStyleName("gwt-Label-header");
+		headerPanel.add(lblHeader);
+
+		// Need spacer to clear the "float: left;" css attribute
+		SimplePanel simplePanel = new SimplePanel();
+		simplePanel.setStyleName("spacer");
+		headerPanel.add(simplePanel);
+
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+
+		lblLoggedIn = new Label("Logged in as: " + user.getEmail());
+		lblLoggedIn.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
+		vPanel.add(lblLoggedIn);
 	}
 
 	private void addConfig(final ConfigProxy conf) {
@@ -95,11 +136,18 @@ public class RequestEntryPoint implements EntryPoint {
 
 		ConfigTable cTab = new ConfigTable();
 		cTab.setPatientData(data);
-		vPanel.add(cTab);
+
+		DecoratorPanel cTabPanel = new DecoratorPanel();
+		cTabPanel.add(cTab);
+		cTabPanel.setWidth("auto");
+		vPanel.add(cTabPanel);
 
 		final DataTable table = new DataTable();
 		table.setText(0, 0, "Finding data for patient " + conf.getPatientName());
-		vPanel.add(table);
+
+		DecoratorPanel dTabPanel = new DecoratorPanel();
+		dTabPanel.add(table);
+		vPanel.add(dTabPanel);
 
 		// Get data associated with the patient in config
 		mRequestFactory.dataRequest().findDataByEmail(conf.getPatientEmail(), Integer.MAX_VALUE).fire(new Receiver<List<DataProxy>>() {
@@ -113,5 +161,4 @@ public class RequestEntryPoint implements EntryPoint {
 		});
 
 	}
-
 }
