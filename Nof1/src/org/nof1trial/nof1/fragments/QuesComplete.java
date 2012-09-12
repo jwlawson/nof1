b55224a4e45daa99aa4e950138294ae7eeaa5eaa
John Lawson <john@jwlawson.co.uk>
2012-09-12 12:42:49 +0100

    Fixes handling onDismiss if the activity is already finished/finishing
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Nof1/src/org/nof1trial/nof1/fragments/QuesComplete.java
+++ Nof1/src/org/nof1trial/nof1/fragments/QuesComplete.java
@@ -20,12 +20,6 @@
  ******************************************************************************/
 package org.nof1trial.nof1.fragments;
 
-import org.nof1trial.nof1.Keys;
-import org.nof1trial.nof1.R;
-import org.nof1trial.nof1.activities.GraphChooser;
-import org.nof1trial.nof1.activities.HomeScreen;
-import org.nof1trial.nof1.services.FinishedService;
-
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -43,8 +37,15 @@ import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockDialogFragment;
 
+import org.nof1trial.nof1.Keys;
+import org.nof1trial.nof1.R;
+import org.nof1trial.nof1.activities.GraphChooser;
+import org.nof1trial.nof1.activities.HomeScreen;
+import org.nof1trial.nof1.services.FinishedService;
+
 /**
- * Dialog Fragment to show after user has completed questionnaire. Includes options to go back to home screen, view
+ * Dialog Fragment to show after user has completed questionnaire. Includes
+ * options to go back to home screen, view
  * graphs or just exit.
  * 
  * @author John Lawson
@@ -73,20 +74,29 @@ public class QuesComplete extends SherlockDialogFragment {
 
 		View view = inflater.inflate(R.layout.complete_layout, container, false);
 
-		SharedPreferences userPrefs = getActivity().getSharedPreferences(Keys.DEFAULT_PREFS, Context.MODE_PRIVATE);
-		SharedPreferences configPrefs = getActivity().getSharedPreferences(Keys.CONFIG_NAME, Context.MODE_PRIVATE);
-		SharedPreferences schedPrefs = getActivity().getSharedPreferences(Keys.SCHED_NAME, Context.MODE_PRIVATE);
+		SharedPreferences userPrefs = getActivity().getSharedPreferences(Keys.DEFAULT_PREFS,
+				Context.MODE_PRIVATE);
+		SharedPreferences configPrefs = getActivity().getSharedPreferences(Keys.CONFIG_NAME,
+				Context.MODE_PRIVATE);
+		SharedPreferences schedPrefs = getActivity().getSharedPreferences(Keys.SCHED_NAME,
+				Context.MODE_PRIVATE);
 		Resources res = getActivity().getResources();
 
 		TextView thanks = (TextView) view.findViewById(R.id.complete_text_thanks);
-		thanks.setText(res.getText(R.string.thanks) + " " + userPrefs.getString(Keys.DEFAULT_PATIENT_NAME, ""));
+		thanks.setText(res.getText(R.string.thanks) + " "
+				+ userPrefs.getString(Keys.DEFAULT_PATIENT_NAME, ""));
 
 		TextView progress = (TextView) view.findViewById(R.id.complete_text_progress);
-		progress.setText("" + res.getText(R.string.you_are_now) + schedPrefs.getInt(Keys.SCHED_CUMULATIVE_DAY, 0) + res.getText(R.string.out_of)
-				+ (configPrefs.getInt(Keys.CONFIG_PERIOD_LENGTH, 0) * configPrefs.getInt(Keys.CONFIG_NUMBER_PERIODS, 0) * 2));
+		progress.setText(""
+				+ res.getText(R.string.you_are_now)
+				+ schedPrefs.getInt(Keys.SCHED_CUMULATIVE_DAY, 0)
+				+ res.getText(R.string.out_of)
+				+ (configPrefs.getInt(Keys.CONFIG_PERIOD_LENGTH, 0)
+						* configPrefs.getInt(Keys.CONFIG_NUMBER_PERIODS, 0) * 2));
 
 		// If the trial is finished, show some text saying this
-		// SCHED_FINISHED gets set by Scheduler when the last questionnaire noti is posted
+		// SCHED_FINISHED gets set by Scheduler when the last questionnaire noti
+		// is posted
 		// So should be set before the user has completed the questionnaire
 		if (schedPrefs.getBoolean(Keys.SCHED_FINISHED, false)) {
 			TextView finished = (TextView) view.findViewById(R.id.complete_text_finished);
@@ -105,7 +115,8 @@ public class QuesComplete extends SherlockDialogFragment {
 			public void onClick(View v) {
 				// Show graphs
 				TaskStackBuilder builder = TaskStackBuilder.create(getActivity());
-				builder.addNextIntent(new Intent(getActivity(), HomeScreen.class)).addNextIntent(new Intent(getActivity(), GraphChooser.class));
+				builder.addNextIntent(new Intent(getActivity(), HomeScreen.class)).addNextIntent(
+						new Intent(getActivity(), GraphChooser.class));
 				builder.startActivities();
 				getActivity().finish();
 			}
@@ -127,7 +138,8 @@ public class QuesComplete extends SherlockDialogFragment {
 		btnExit.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// Finish activity. As user got here from notification, this should be the only thing in task stack
+				// Finish activity. As user got here from notification, this
+				// should be the only thing in task stack
 				getActivity().finish();
 			}
 		});
@@ -139,8 +151,11 @@ public class QuesComplete extends SherlockDialogFragment {
 	public void onDismiss(DialogInterface dialog) {
 		super.onDismiss(dialog);
 
-		// Finish activity when the dialog is closed to prevent user pressing back, clearing the dialog and being back
+		// Finish activity when the dialog is closed to prevent user pressing
+		// back, clearing the dialog and being back
 		// at data input screen
-		getActivity().finish();
+		if (getActivity() != null && !getActivity().isFinishing()) {
+			getActivity().finish();
+		}
 	}
 }
