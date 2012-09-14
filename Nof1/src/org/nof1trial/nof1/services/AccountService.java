f2d198d6554d855a85a1b9653078c48a6b6eb4e9
John Lawson <john@jwlawson.co.uk>
2012-09-14 10:44:45 +0100

    Fixes null intent on notification
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Nof1/src/org/nof1trial/nof1/services/AccountService.java
+++ Nof1/src/org/nof1trial/nof1/services/AccountService.java
@@ -20,24 +20,10 @@
  ******************************************************************************/
 package org.nof1trial.nof1.services;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.accounts.AccountManagerCallback;
-import android.accounts.AccountManagerFuture;
-import android.accounts.AuthenticatorException;
-import android.accounts.OperationCanceledException;
-import android.app.IntentService;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Resources;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationCompat.Builder;
-import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
 
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
@@ -53,10 +39,25 @@ import org.nof1trial.nof1.R;
 import org.nof1trial.nof1.activities.AccountsActivity;
 import org.nof1trial.nof1.app.Util;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLEncoder;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.Builder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
 /**
  * Service to handle registering accounts. Can register an account for the first
@@ -92,14 +93,15 @@ public class AccountService extends IntentService {
 	public void onCreate() {
 		super.onCreate();
 
-		Resources res = getResources();
+		final Resources res = getResources();
 		if (DEBUG) Log.d(TAG, "Account service started");
 
+		final PendingIntent empty = PendingIntent.getActivity(getApplicationContext(), 0, new Intent(), 0);
+
 		// Show notification
-		NotificationCompat.Builder builder = new Builder(mContext);
-		builder.setContentTitle(res.getString(R.string.nof1_account_setup))
-				.setContentText(res.getString(R.string.retrieve_account)).setAutoCancel(false)
-				.setSmallIcon(R.drawable.noti_account).setProgress(0, 0, true);
+		final NotificationCompat.Builder builder = new Builder(mContext);
+		builder.setContentTitle(res.getString(R.string.nof1_account_setup)).setContentText(res.getString(R.string.retrieve_account))
+				.setContentIntent(empty).setAutoCancel(false).setSmallIcon(R.drawable.noti_account).setProgress(0, 0, true);
 
 		mNM = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 		mNM.notify(NOTIFICATION_ID, builder.build());
@@ -169,35 +171,32 @@ public class AccountService extends IntentService {
 					// Use a fake cookie for the dev mode app engine server
 					// The cookie has the form email:isAdmin:userId
 					// We set the userId to be the same as the account name
-					String authCookie = "dev_appserver_login=" + accountName + ":false:"
-							+ accountName;
+					String authCookie = "dev_appserver_login=" + accountName + ":false:" + accountName;
 					prefs.edit().putString(Util.AUTH_COOKIE, authCookie).commit();
 				} else {
 					// Get the auth token from the AccountManager and convert
 					// it into a cookie for the appengine server
-					mgr.getAuthToken(acct, "ah", null, new AccountsActivity(),
-							new AccountManagerCallback<Bundle>() {
-								@Override
-								public void run(AccountManagerFuture<Bundle> future) {
-									try {
-										Bundle authTokenBundle = future.getResult();
-										String authToken = authTokenBundle.get(
-												AccountManager.KEY_AUTHTOKEN).toString();
+					mgr.getAuthToken(acct, "ah", null, new AccountsActivity(), new AccountManagerCallback<Bundle>() {
+						@Override
+						public void run(AccountManagerFuture<Bundle> future) {
+							try {
+								Bundle authTokenBundle = future.getResult();
+								String authToken = authTokenBundle.get(AccountManager.KEY_AUTHTOKEN).toString();
 
-										new CookieSaver().execute(authToken);
+								new CookieSaver().execute(authToken);
 
-									} catch (AuthenticatorException e) {
-										Log.w(TAG, "Got AuthenticatorException " + e);
-										Log.w(TAG, Log.getStackTraceString(e));
-									} catch (IOException e) {
-										Log.w(TAG, "Got IOException " + Log.getStackTraceString(e));
-										Log.w(TAG, Log.getStackTraceString(e));
-									} catch (OperationCanceledException e) {
-										Log.w(TAG, "Got OperationCanceledException " + e);
-										Log.w(TAG, Log.getStackTraceString(e));
-									}
-								}
-							}, null);
+							} catch (AuthenticatorException e) {
+								Log.w(TAG, "Got AuthenticatorException " + e);
+								Log.w(TAG, Log.getStackTraceString(e));
+							} catch (IOException e) {
+								Log.w(TAG, "Got IOException " + Log.getStackTraceString(e));
+								Log.w(TAG, Log.getStackTraceString(e));
+							} catch (OperationCanceledException e) {
+								Log.w(TAG, "Got OperationCanceledException " + e);
+								Log.w(TAG, Log.getStackTraceString(e));
+							}
+						}
+					}, null);
 				}
 				break;
 			}
@@ -236,8 +235,7 @@ public class AccountService extends IntentService {
 					// Use a fake cookie for the dev mode app engine server
 					// The cookie has the form email:isAdmin:userId
 					// We set the userId to be the same as the account name
-					String authCookie = "dev_appserver_login=" + accountName + ":false:"
-							+ accountName;
+					String authCookie = "dev_appserver_login=" + accountName + ":false:" + accountName;
 					prefs.edit().putString(Util.AUTH_COOKIE, authCookie).commit();
 				} else {
 					// Get the auth token from the AccountManager and convert
@@ -254,41 +252,34 @@ public class AccountService extends IntentService {
 						public void run(AccountManagerFuture<Bundle> future) {
 							try {
 								Bundle authTokenBundle = future.getResult();
-								String authToken = authTokenBundle
-										.get(AccountManager.KEY_AUTHTOKEN).toString();
+								String authToken = authTokenBundle.get(AccountManager.KEY_AUTHTOKEN).toString();
 
 								// Invalidate auth token, so next time Account
 								// Manager requests a new one
 								mgr.invalidateAuthToken("com.google", authToken);
 
 								// Request new one
-								mgr.getAuthToken(acct, "ah", null, act,
-										new AccountManagerCallback<Bundle>() {
-											@Override
-											public void run(AccountManagerFuture<Bundle> future) {
-												try {
-													Bundle authTokenBundle = future.getResult();
-													String authToken = authTokenBundle.get(
-															AccountManager.KEY_AUTHTOKEN)
-															.toString();
+								mgr.getAuthToken(acct, "ah", null, act, new AccountManagerCallback<Bundle>() {
+									@Override
+									public void run(AccountManagerFuture<Bundle> future) {
+										try {
+											Bundle authTokenBundle = future.getResult();
+											String authToken = authTokenBundle.get(AccountManager.KEY_AUTHTOKEN).toString();
 
-													new CookieSaver().execute(authToken);
+											new CookieSaver().execute(authToken);
 
-												} catch (AuthenticatorException e) {
-													Log.w(TAG, "Got AuthenticatorException " + e);
-													Log.w(TAG, Log.getStackTraceString(e));
-												} catch (IOException e) {
-													Log.w(TAG,
-															"Got IOException "
-																	+ Log.getStackTraceString(e));
-													Log.w(TAG, Log.getStackTraceString(e));
-												} catch (OperationCanceledException e) {
-													Log.w(TAG, "Got OperationCanceledException "
-															+ e);
-													Log.w(TAG, Log.getStackTraceString(e));
-												}
-											}
-										}, null);
+										} catch (AuthenticatorException e) {
+											Log.w(TAG, "Got AuthenticatorException " + e);
+											Log.w(TAG, Log.getStackTraceString(e));
+										} catch (IOException e) {
+											Log.w(TAG, "Got IOException " + Log.getStackTraceString(e));
+											Log.w(TAG, Log.getStackTraceString(e));
+										} catch (OperationCanceledException e) {
+											Log.w(TAG, "Got OperationCanceledException " + e);
+											Log.w(TAG, Log.getStackTraceString(e));
+										}
+									}
+								}, null);
 
 							} catch (AuthenticatorException e) {
 								Log.w(TAG, "Got AuthenticatorException " + e);
@@ -318,8 +309,7 @@ public class AccountService extends IntentService {
 			// Get SACSID cookie
 			DefaultHttpClient client = new DefaultHttpClient();
 			String continueURL = Util.PROD_URL;
-			URI uri = new URI(Util.PROD_URL + "/_ah/login?continue="
-					+ URLEncoder.encode(continueURL, "UTF-8") + "&auth=" + authToken);
+			URI uri = new URI(Util.PROD_URL + "/_ah/login?continue=" + URLEncoder.encode(continueURL, "UTF-8") + "&auth=" + authToken);
 			HttpGet method = new HttpGet(uri);
 			final HttpParams getParams = new BasicHttpParams();
 			HttpClientParams.setRedirecting(getParams, false);
