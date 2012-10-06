74a20b887ac27540429ac3f2d56c5a7667b1dbe3
John Lawson <john@jwlawson.co.uk>
2012-10-06 18:02:37 +0100

    RPC stuff to get an upload url from gae blobstore
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

---
+++ Nof1-AppEngine/src/org/nof1trial/nof1/server/services/UploadServiceImpl.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Nof1 Trials helper, making life easier for clinicians and patients in N of 1 trials.
+ * Copyright (C) 2012  John Lawson
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You may obtain a copy of the GNU General Public License at 
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     John Lawson - initial API and implementation
+ ******************************************************************************/
+package org.nof1trial.nof1.server.services;
+
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import org.nof1trial.nof1.client.services.UserUploadService;
+
+/**
+ * @author John
+ * 
+ */
+@SuppressWarnings("serial")
+public class UploadServiceImpl extends RemoteServiceServlet implements UserUploadService {
+
+	@Override
+	public String getBlobstoreUploadUrl() {
+		BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+		return blobstoreService.createUploadUrl("/upload");
+	}
+}
