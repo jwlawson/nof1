e0e02965babaa1d68ec048a44bc2ac2d2d87ae1f
John Lawson <john@jwlawson.co.uk>
2012-10-06 18:03:12 +0100

    Adds upload module to handle uploading file
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

---
+++ Nof1-AppEngine/src/org/nof1trial/nof1/client/UploadEntryPoint.java
@@ -0,0 +1,139 @@
+/*******************************************************************************
+ * Nof1 Trials helper, making life easier for clinicians and patients in N of 1 trials.
+ * Copyright (C) 2012  John Lawson
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You may obtain a copy of the GNU General Public License at 
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     John Lawson - initial API and implementation
+ ******************************************************************************/
+package org.nof1trial.nof1.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.SimpleEventBus;
+
+import org.nof1trial.nof1.client.services.UserUploadService;
+import org.nof1trial.nof1.client.services.UserUploadServiceAsync;
+import org.nof1trial.nof1.shared.MyRequestFactory;
+
+/**
+ * @author John
+ * 
+ */
+public class UploadEntryPoint implements EntryPoint {
+
+	private MyRequestFactory mRequestFactory;
+
+	private FlowPanel vPanel;
+	private Button btnSubmit;
+	private FormPanel uploadForm;
+	UserUploadServiceAsync uploadService = GWT.create(UserUploadService.class);
+
+	@Override
+	public void onModuleLoad() {
+
+		final EventBus eventBus = new SimpleEventBus();
+
+		mRequestFactory = GWT.create(MyRequestFactory.class);
+		mRequestFactory.initialize(eventBus);
+
+		createUi();
+	}
+
+	private void createUi() {
+		vPanel = new FlowPanel();
+		vPanel.setStyleName("vPanel");
+
+		RootPanel rootPanel = RootPanel.get();
+		rootPanel.add(vPanel);
+
+		FlowPanel headerPanel = new FlowPanel();
+		headerPanel.setStyleName("headerPanel");
+		vPanel.add(headerPanel);
+
+		// Nof1 logo header
+		Image imgHeader = new Image("images/Nof1-Icon.png");
+		imgHeader.setStyleName("gwt-Image-header");
+		imgHeader.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				Window.Location.assign("http://www.nof1trial.org");
+			}
+		});
+		headerPanel.add(imgHeader);
+
+		// Header text
+		Label lblHeader = new Label("User data");
+		lblHeader.setStyleName("gwt-Label-header");
+		headerPanel.add(lblHeader);
+
+		// Need spacer to clear the "float: left;" css attribute
+		SimplePanel simplePanel = new SimplePanel();
+		simplePanel.setStyleName("spacer");
+		headerPanel.add(simplePanel);
+
+		uploadForm = new FormPanel();
+		uploadForm.setEncoding(FormPanel.ENCODING_MULTIPART);
+		uploadForm.setMethod(FormPanel.METHOD_POST);
+		vPanel.add(uploadForm);
+
+		FileUpload fileUpload = new FileUpload();
+		uploadForm.setWidget(fileUpload);
+		fileUpload.setSize("100%", "100%");
+		fileUpload.setName("file");
+
+		btnSubmit = new Button("Loading...");
+		vPanel.add(btnSubmit);
+
+		getUploadUrl();
+	}
+
+	private void getUploadUrl() {
+		uploadService.getBlobstoreUploadUrl(new AsyncCallback<String>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+			}
+
+			@Override
+			public void onSuccess(String result) {
+				btnSubmit.setText("Submit");
+				btnSubmit.addClickHandler(new ClickHandler() {
+
+					@Override
+					public void onClick(ClickEvent event) {
+						uploadForm.submit();
+					}
+
+				});
+			}
+
+		});
+	}
+}
